d8af50e61c29cc47459fbfdb17d779bc8dd2cc1a
Authis <authis.sundar@gmail.com>
2019-04-14 11:38:28 +0100

    Created LOMS Repository

---
+++ src/com/action/LomsCommon.java
@@ -0,0 +1,135 @@
+package com.action;
+
+import java.io.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.mail.MessagingException;
+
+import oracle.jdbc.OracleCallableStatement;
+import oracle.jdbc.OracleTypes;
+
+import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.mail.SendMailUsingAuthentication;
+import com.opensymphony.xwork2.ActionSupport; 
+import com.bean.LoginBean;
+import com.bean.ProductSetBean;
+import com.doa.ClientInfoDoa;
+import com.doa.LomsCommonDoa;
+ 
+
+public class LomsCommon extends ActionSupport implements SessionAware {
+	
+	
+	public Map session; // ActionContext.getContext().getSession();
+ 
+	LoginLoms loginLoms = new LoginLoms();
+	LoginBean loginbean = new LoginBean();
+	String retEmpType = "";  // Variable used to Restricting menu for USER and ADMIN 
+	
+	String retMsg = "";
+	
+	LomsCommonDoa lomsCommonDoa = new LomsCommonDoa();
+	ProductSetBean productSetBean = new ProductSetBean();
+	
+	public ProductSetBean getProductSetBean() {
+		return productSetBean;
+	}
+
+
+
+	public void setProductSetBean(ProductSetBean productSetBean) {
+		this.productSetBean = productSetBean;
+	}
+
+
+
+	public String getRetMsg() {
+		return retMsg;
+	}
+
+
+
+	public void setRetMsg(String retMsg) {
+		this.retMsg = retMsg;
+	}
+
+
+
+	public String getRetEmpType() {
+		return retEmpType;
+	}
+
+
+
+	public void setRetEmpType(String retEmpType) {
+		this.retEmpType = retEmpType;
+	}
+
+
+
+	public String editProductSet() {
+
+		String username = "";
+		String password = "";
+		retEmpType = (String) session.get("retEmpType"); // Variable used to Restricting menu for USER and ADMIN 
+		loginbean = (LoginBean) session.get("loginbean");
+		String ret = "";
+		if (loginbean != null) {
+			  username = loginbean.getUserName();
+			  password = loginbean.getPassWord();
+
+			//ret = loginLoms.execute(username, password);
+			
+		}
+		if ( loginbean != null) {
+			
+			productSetBean = lomsCommonDoa.editProductSetDoa(username, password);
+			
+			return "SUCCESS";
+		} else {
+			
+			return "FAILURE";
+		}
+	}
+	
+	public String updateProdSet() {
+
+		String username = "";
+		String password = "";
+		retEmpType = (String) session.get("retEmpType"); // Variable used to Restricting menu for USER and ADMIN 
+		loginbean = (LoginBean) session.get("loginbean");
+		String ret = "";
+		if (loginbean != null) {
+			  username = loginbean.getUserName();
+			  password = loginbean.getPassWord();
+
+			// ret = loginLoms.execute(username, password);
+			
+		}
+		if (  loginbean != null) {
+			
+			retMsg = lomsCommonDoa.updateProdSetDoa(productSetBean,username, password);
+			
+			return "SUCCESS";
+		} else {
+			
+			return "FAILURE";
+		}
+	}
+	
+	
+	
+	
+
+	@Override
+	public void setSession(Map arg0) {
+		this.session = arg0;
+
+	}
+
+
+}
